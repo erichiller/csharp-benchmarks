c7e180ca3f4bd8c83d4170783eb064b2ad433001
Eric D. Hiller <eric@hiller.pro>
2022-06-27 07:48:19 -0500

    Notes, WhenAll test

--- LanguageTests/AsyncTests.cs
+++ LanguageTests/AsyncTests.cs
@@ -17,11 +17,13 @@ public class AsyncTests : TestBase<AsyncTests> {
     private static async Task doSomeWork( ) => await Task.Delay( 1000 );
     
     /* Results
-        07:16:06.716 -05:00 [INF] 21 >> Inline x3 took: "00:00:03.0001159"
-        07:16:07.743 -05:00 [INF] 20 >> Await After Task start x3 took: "00:00:01.0149079"
-        07:16:08.746 -05:00 [INF] 20 >> WaitAll with inline tasks x3 took: "00:00:01.0027456"
-        07:16:09.745 -05:00 [INF] 20 >> WaitAll After Task start x3 took: "00:00:00.9986269"
+        Inline x3 took: "00:00:03.0001159"
+        Await After Task start x3 took: "00:00:01.0149079"
+        WaitAll with inline tasks x3 took: "00:00:01.0027456"
+        WaitAll After Task start x3 took: "00:00:00.9986269"
+        WhenAll with inline tasks x3 took: "00:00:00.9976685"
      */
+    // see: https://alt-dev.com.au/why-you-may-be-doing-async-wrong-in-c
     [ Fact ]
     public async Task InlineAsyncShouldBeSlowerThanSplit( ) {
 
@@ -53,6 +55,10 @@ public class AsyncTests : TestBase<AsyncTests> {
         var t3B = doSomeWork();
         Task.WaitAll( t1B, t2B, t3B );
         this._logger.LogInformation( "WaitAll After Task start x3 took: {Stopwatch}", stopwatch.Elapsed );
+        
+        stopwatch.Restart();
+        await Task.WhenAll( doSomeWork(), doSomeWork(), doSomeWork() );
+        this._logger.LogInformation( "WhenAll with inline tasks x3 took: {Stopwatch}", stopwatch.Elapsed );
 
 
 
